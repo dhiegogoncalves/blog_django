5bc9f5b5454c846a15207d3ed870a9fdd00e43a6
Dhiego Gonçalves da Silva <dhhiego@gmail.com>
2019-08-02 16:37:53 -0400

    Implementação da Model Post

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,32 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='publicado')
+
+
+class Post(models.Model):
+    STATUS = (
+        ('rasculho', 'Rasculho'),
+        ('publicado', 'Publicado')
+    )
+    titulo = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    autor = models.ForeignKey(User, on_delete=models.CASCADE)
+    conteudo = models.TextField()
+    publicado = models.DateTimeField(default=timezone.now)
+    criado = models.DateTimeField(auto_now_add=True)
+    alterado = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS, default='rasculho')
+
+    objects = models.Manager()
+    published = PublishedManager()
+
+    class Meta:
+        ordering = ('-publicado',)
+
+    def __str__(self):
+        return self.titulo
