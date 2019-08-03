cc346691f19be32dd215d97eb8803e89a04dd142
Dhiego Gonçalves da Silva <dhhiego@gmail.com>
2019-08-03 18:29:25 -0400

    Implementação do CRUD do Post

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.urls import reverse
 from django.utils import timezone
+from django.utils.text import slugify
 
 
 class PublishedManager(models.Manager):
@@ -25,8 +29,27 @@ class Post(models.Model):
     objects = models.Manager()
     published = PublishedManager()
 
+    def get_absolute_url(self):
+        return reverse('home')
+
+    def get_absolute_url_detail(self):
+        return reverse('post_detail', args=[self.slug])
+
+    def get_absolute_url_update(self):
+        return reverse('post_edit', args=[self.pk])
+
+    def get_absolute_url_delete(self):
+        return reverse('post_delete', args=[self.pk])
+
     class Meta:
         ordering = ('-publicado',)
 
     def __str__(self):
         return self.titulo
+
+
+@receiver(post_save, sender=Post)
+def insert_slug(sender, instance, **kwargs):
+    if not instance.slug:
+        instance.slug = slugify(instance.titulo)
+        return instance.save()
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
-# Create your views here.
+from .models import Post
+
+
+class BlogListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+
+
+class BlogCreateView(SuccessMessageMixin, CreateView):
+    model = Post
+    fields = ['autor', 'titulo', 'conteudo']
+    template_name = 'blog/post_new.html'
+    # context_object_name = 'post'
+    success_message = "%(field)s - criado com sucesso"
+
+    def get_success_message(self, cleaned_data):
+        return self.success_message % dict(
+            cleaned_data,
+            field=self.object.titulo,
+        )
+
+
+class BlogUpdateView(SuccessMessageMixin, UpdateView):
+    model = Post
+    fields = ['autor', 'titulo', 'conteudo']
+    template_name = 'blog/post_edit.html'
+    success_message = "%(field)s - alterado com sucesso"
+
+    def get_success_message(self, cleaned_data):
+        return self.success_message % dict(
+            cleaned_data,
+            field=self.object.titulo,
+        )
+
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+
+
+class BlogDeleteView(SuccessMessageMixin, DeleteView):
+    model = Post
+    template_name = 'blog/post_delete.html'
+    success_url = reverse_lazy('home')
+    success_message = "Deletado com sucesso"
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, self.success_message)
+        return super(BlogDeleteView, self).delete(request, *args, **kwargs)
